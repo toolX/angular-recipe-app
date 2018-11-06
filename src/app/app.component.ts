fc47507b93287e3e7fcd62194352162dc4bca840
Philip Shamsiev <Philip_Shamsiev@epam.com>
2018-11-06 15:31:14 +0300

    Initialize firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyCaNdJTY39Q-ugOPrQ9xgXYZImTPjxNd0A',
+      authDomain: 'max-http-89c28.firebaseapp.com',
+    });
+  }
+}
