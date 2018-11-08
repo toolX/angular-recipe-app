f93ad535f63f183991d46d2b86f927f1245aadb6
Philip Shamsiev <Philip_Shamsiev@epam.com>
2018-11-08 11:31:22 +0300

    Lazy-loading recipes module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,6 @@ import { RecipeService } from './recipes/recipe.service';
 import { DataStorageService } from './shared/data-storage.service';
 import { AuthService } from './auth/auth.service';
 import { AuthGuard } from './auth/auth-guard.service';
-import { RecipesModule } from './recipes/recipes.module';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { AuthModule } from './auth/auth.module';
@@ -28,7 +27,6 @@ import { HomeComponent } from './home/home.component';
     BrowserModule,
     HttpModule,
     HttpClientModule,
-    RecipesModule,
     ShoppingListModule,
     AuthModule,
     AppRoutingModule,
