f8b67bae061ef030a9e6370336f029456e31960e
Philip Shamsiev <Philip_Shamsiev@epam.com>
2018-11-08 11:42:56 +0300

    Create core module to split code; move home component and header component into the core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { HttpModule } from '@angular/http';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 
 import { ShoppingListService } from './shopping-list/shopping-list.service';
@@ -15,13 +14,11 @@ import { AuthGuard } from './auth/auth-guard.service';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { AuthModule } from './auth/auth.module';
-import { HomeComponent } from './home/home.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    HomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
@@ -30,7 +27,8 @@ import { HomeComponent } from './home/home.component';
     ShoppingListModule,
     AuthModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    CoreModule
   ],
   providers: [
     ShoppingListService, 
