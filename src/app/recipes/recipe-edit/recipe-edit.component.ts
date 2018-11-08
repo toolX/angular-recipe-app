673cb2e225d0b1b874f6472e2192ebb3c272fb98
Philip Shamsiev <Philip_Shamsiev@epam.com>
2018-11-08 12:04:33 +0300

    Create getControls method

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -92,4 +92,8 @@ export class RecipeEditComponent implements OnInit {
       'ingredients': recipeIngredients
     });
   }
+
+  getControls() {
+    return (<FormArray>this.recipeForm.get('ingredients')).controls;
+  }
 }
