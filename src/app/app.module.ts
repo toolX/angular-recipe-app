080364c306cf36e68235308a823fe82ef08ab358
Philip Shamsiev <Philip_Shamsiev@epam.com>
2018-11-08 11:57:30 +0300

    Move providers' array into the core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,6 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
-import { ShoppingListService } from './shopping-list/shopping-list.service';
-import { RecipeService } from './recipes/recipe.service';
-import { DataStorageService } from './shared/data-storage.service';
-import { AuthService } from './auth/auth.service';
-import { AuthGuard } from './auth/auth-guard.service';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { AuthModule } from './auth/auth.module';
@@ -30,13 +24,7 @@ import { CoreModule } from './core/core.module';
     SharedModule,
     CoreModule
   ],
-  providers: [
-    ShoppingListService, 
-    RecipeService, 
-    DataStorageService, 
-    AuthService, 
-    AuthGuard
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,6 +5,11 @@ import { HomeComponent } from "./home/home.component";
 import { SharedModule } from "../shared/shared.module";
 import { AppRoutingModule } from "../app-routing.module";
 
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
+import { RecipeService } from "../recipes/recipe.service";
+import { DataStorageService } from "../shared/data-storage.service";
+import { AuthService } from "../auth/auth.service";
+
 @NgModule({
   declarations: [
     HeaderComponent,
@@ -17,6 +22,12 @@ import { AppRoutingModule } from "../app-routing.module";
   exports: [
     AppRoutingModule,
     HeaderComponent
+  ],
+  providers: [
+    ShoppingListService,
+    RecipeService,
+    DataStorageService,
+    AuthService
   ]
 })
 export class CoreModule {}
